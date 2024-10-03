013e0accefc5dec103b382c4ce818e653179d7ce
Ferdaws <147564956+Ahmad986Ferdaws@users.noreply.github.com>
2024-10-03 17:08:55 -0500

    Create employee_info.c

---
+++ employee_info.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DATA_SIZE 1000
+
+int main() {
+    char name[50];
+    int age;
+    float salary;
+
+    FILE *filePtr = NULL;
+
+    filePtr = fopen("emp_records.txt", "w");
+
+    if (filePtr == NULL) {
+        printf("Error opening file!\n");
+        return 1;
+    }
+
+    printf("Please enter the name of the employee: ");
+    fgets(name, sizeof(name), stdin);
+
+    printf("Please enter the age of the employee: ");
+    scanf("%d", &age);
+
+    printf("Please enter the salary of the employee: ");
+    scanf("%f", &salary);
+
+    fprintf(filePtr, "Name: %s", name);
+    fprintf(filePtr, "Age: %d\n", age);
+    fprintf(filePtr, "Salary: %.2f\n", salary);
+
+    fclose(filePtr);
+
+    printf("Employee records have been saved.\n");
+
+    return 0;
+}
