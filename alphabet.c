1c37822d05187b768b83fd1b4858754986587435
Ferdaws <147564956+Ahmad986Ferdaws@users.noreply.github.com>
2024-10-03 17:09:22 -0500

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    FILE *filePtr;
+    char ch;
+
+    filePtr = fopen("alphabet.txt", "w");
+    if (filePtr == NULL) {
+        printf("Error opening file for writing.\n");
+        return 1;
+    }
+
+    for (ch = 'A'; ch <= 'Z'; ch++) {
+        fputc(ch, filePtr);
+    }
+    fclose(filePtr);
+
+    filePtr = fopen("alphabet.txt", "r");
+    if (filePtr == NULL) {
+        printf("Error opening file for reading.\n");
+        return 1;
+    }
+
+    printf("Characters in lowercase:\n");
+    while ((ch = fgetc(filePtr)) != EOF) {
+        printf("%c ", tolower(ch));
+    }
+    fclose(filePtr);
+
+    return 0;
+}
