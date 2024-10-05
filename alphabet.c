b650ba228978cac31e7d3b23332515edd0c07a6d
Ferdaws <147564956+Ahmad986Ferdaws@users.noreply.github.com>
2024-10-04 22:59:31 -0500

    Update alphabet.c

--- alphabet.c
+++ alphabet.c
@@ -2,6 +2,7 @@
 #include <ctype.h>
 // so this is one of the important projects
 int main() {
+    // in this part we use the to point out the address of the file
     FILE *filePtr;
     char ch;
 
