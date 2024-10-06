9de1c9666d86be82703367d888a6373a4f8c1354
Ferdaws <147564956+Ahmad986Ferdaws@users.noreply.github.com>
2024-10-06 12:09:01 -0500

    Update sum_int.c

--- sum_int.c
+++ sum_int.c
@@ -5,7 +5,7 @@ int main() {
     FILE *filePtr;
     int num[] = {0,1,2,3,4,5,6,7,8,9,10};
     int soom = 5;
-
+    // Using the W function in We open a file in C
     filePtr = fopen("average_sum.txt", "w");
     if (filePtr == NULL) {
         printf("Error opening file for writing.\n");
