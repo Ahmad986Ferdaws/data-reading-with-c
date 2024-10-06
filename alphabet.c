d325cbf9bcca89f6ce953a12c9dcbc9dd2a0207d
Ferdaws <147564956+Ahmad986Ferdaws@users.noreply.github.com>
2024-10-05 23:40:10 -0500

    Update alphabet.c

--- alphabet.c
+++ alphabet.c
@@ -3,7 +3,7 @@
 // so this is one of the important projects
 int main() {
     // in this part we use the to point out the address of the file sdj
-    // hello this is the thing that i 
+    // hello this is the thing that about the 
     FILE *filePtr;
     char ch;
     // we use the W function to open the text file
